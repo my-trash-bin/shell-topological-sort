b1998db8e47b6dcb44b0aa5fa130005f073e64e6
Juyeong Maing <mjy9088@naver.com>
2023-07-04 16:16:11 +0100

    initial commit

---
+++ main.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+TMP_DIR="tmp.$$"
+
+cleanup() {
+  rm -rf "$TMP_DIR"
+  exit 1
+}
+trap cleanup EXIT
+trap cleanup INT
+trap cleanup TERM
+
+encode_octal() {
+  STRING="$1"
+  RESULT=""
+  for c in $(printf "%s" "$STRING" | od -An -v -t o1 | tr -d ' '); do
+    RESULT="$RESULT$c"
+  done
+  echo "$RESULT"
+}
+
+mkdir -p "$TMP_DIR/input"
+while IFS="=" read -r dependent dependencies; do
+  echo "$dependencies" | xargs -n 1 echo > "$TMP_DIR/input/$(encode_octal "$dependent")"
+  echo "$dependencies" | xargs -n 1 echo | while IFS= read -r line; do
+    touch "$TMP_DIR/input/$(encode_octal "$line")"
+  done
+done
+
+mkdir -p "$TMP_DIR/process"
+(cd "$TMP_DIR/input" && echo * | xargs -n 1 echo | grep -v '^*$') | sort | while IFS= read -r line; do
+  TMP_DIR="$TMP_DIR" sh "$(dirname "$0")/sub.sh" "$line"
+done
+
+[ ! -f "$TMP_DIR/out.txt" ] || cat "$TMP_DIR/out.txt"
---
+++ sub.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+encode_octal() {
+  STRING="$1"
+  RESULT=""
+  for c in $(printf "%s" "$STRING" | od -An -v -t o1 | tr -d ' '); do
+    RESULT="$RESULT$c"
+  done
+  echo "$RESULT"
+}
+
+decode_octal() {
+  STRING="$1"
+  RESULT=""
+  while [ -n "$STRING" ]; do
+    c=$(printf "%s\n" "$STRING" | cut -c 1-3)
+    # shellcheck disable=SC2059
+    RESULT="$RESULT$(printf "\\$c")"
+    STRING=$(printf "%s\n" "$STRING" | cut -c 4-)
+  done
+  echo "$RESULT"
+}
+
+[ -f "$TMP_DIR/process/$1" ] || {
+  touch "$TMP_DIR/process/$1"
+  < "$TMP_DIR/input/$1" grep -v '^$' | while IFS= read -r line; do
+    sh "$0" "$(encode_octal "$line")"
+  done
+  decode_octal "$1" >> "$TMP_DIR/out.txt"
+}
